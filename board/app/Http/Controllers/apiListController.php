cd9e9dc701f21770d282736eb8f2a3d7e69ab124
xellos88 <parkjy0419@naver.com>
2023-06-01 18:07:49 +0900

    0601

--- board/app/Http/Controllers/apiListController.php
+++ board/app/Http/Controllers/apiListController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Boards;
+use Illuminate\Support\Facades\Validator;
 
 class apiListController extends Controller
 {
@@ -27,4 +28,89 @@ class apiListController extends Controller
         return $arr;
 
     }
+
+    function putlist(Request $req, $id)
+    {
+        $arrData=[
+            'code'          => '0'
+            ,'msg'          => ''
+            // ,'org_data'     => []
+            // ,'udt_data'     => []
+        ];
+
+        $data = $req->only('title','content');
+        $data['id']=$id;
+
+        $validator = Validator::make($data,[
+            'id'        => 'required|integer|exists:boards'
+            ,'title'    => 'required|between:3,30'
+            ,'content'  => 'required|max:2000'
+        ]);
+
+        if($validator->fails()){
+            $arrData['code'] ='E01';
+            $arrData['msg'] = 'Validate Error';
+            $arrData['errmsg'] = $validator->errors()->all();
+            return $arrData;
+        } else {
+            //업데이트처리
+            $boards = Boards::find($id);
+            $boards->title =$req->title;
+            $boards->content =$req->content;
+            $boards->save();
+
+            $arrData['code'] ='0';
+            $arrData['msg'] = 'Success';
+
+        }
+        return $arrData;
+        
+
+        // 유효성체크
+        // $req->validate([
+        //     'title' => 'required',
+        //     'content' => 'required'
+        // ]);
+        // $boards = Boards::find($id);
+
+        // if ($boards) {
+        //     $boards->title = $req->title;
+        //     $boards->content = $req->content;
+        //     $boards->save();
+
+        //     $arr['errorcode'] = '0';
+        //     $arr['msg'] = 'success';
+        //     $arr['data'] = $boards->only('id', 'title');
+
+        //     return $arr;
+        // }
+
+        // return response()->json([$boards],200);
+    }
+
+    public function deletelist($id)
+    {
+        $validator = Validator::make(['id' => $id], [
+            'id' => 'required|integer|exists:boards'
+        ]);
+    
+        if ($validator->fails()) {
+            $arrData['code'] = 'E01';
+            $arrData['msg'] = 'Validation Error';
+            $arrData['errmsg'] = $validator->errors()->all();
+            return $arrData;
+        }
+    
+        $boards = Boards::destroy($id);
+    
+        if ($boards) {
+            $arr['errorcode'] = '0';
+            $arr['msg'] = 'success';
+            $arr['id'] = $id;
+            return $arr;
+        }
+    
+        return response()->json(['error' => 'Board not found'], 404);
+    }
+    
 }
